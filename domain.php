77fa23eae6c48e0734c603f0b24cecd24fa3b738
Jan Cordeiro <121439832+jancordeiro@users.noreply.github.com>
2023-05-04 13:38:10 -0300

    Update domain.php

--- domain.php
+++ domain.php
@@ -1,66 +1,46 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
     <meta charset="UTF-8"/>
-<title>Domain Checker 1.0</title>
-<style>
-body {
-  background-color: #212121;
-  text-align: center;
-  color: #fff000;
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-.domainphp {
-    font-family: Arial, Helvetica, sans-serif;
-    font-size: 20px;
-}
-
-a.backbtn {
-  background-color: #fff000;
-  color: #000;
-  font-weight: bold;
-  border: none;
-  padding: 8px 16px;
-  border-radius: 4px;
-  cursor: pointer;
-}
-
-a.backbtn:hover {
-  background-color: #000;
-  color: #fff;
-  font-weight: bold;
-}
-</style>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE-edge">
+	<meta name="viewport" content="width=devide-width, initial-scale=1.0">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Domain Checker 1.0</title>
 </head>
 <body>
-<h1>Domain Checker 1.0</h1>
-<p>By Jan Cordeiro</p><br />
-<div class="domainphp">
+	<div class="container">
+		<div class="box">
+		<h1>Domain Checker 1.0</h1>
+		<br><br>
+			<div class="domainphp">
 
-<?php
-if(isset($_POST['submit'])) {
-    $domain = $_POST['domain'];
-    $recordTypes = array("A", "MX", "NS", "SOA", "TXT");
-    $disponivel = true;
+				<?php
+				if(isset($_POST['submit'])) {
+					$domain = $_POST['domain'];
+					$recordTypes = array("A", "MX", "NS", "SOA", "TXT");
+					$disponivel = true;
 
-    foreach ($recordTypes as $recordType) {
-        if (checkdnsrr($domain, $recordType)) {
-            $disponivel = false;
-            break;
-        }
-    }
+					foreach ($recordTypes as $recordType) {
+						if (checkdnsrr($domain, $recordType)) {
+							$disponivel = false;
+							break;
+						}
+					}
 
-    if($disponivel){
-        echo "<p style='color:green;'>The <u>$domain</u> domain is available to register!</p>";
-    }else{
-        echo "<p style='color:red;'>The <u>$domain</u> domain is not available!</p>";
-    }
-}
-?>
-
-</div>
-<div><p><a class="backbtn" href="javascript:history.back()">VOLTAR</a></p><br />
-<p><a href="https://github.com/jancordeiro" target="_blank"><img src="github-mark-white.png" width="35" height="35" alt="Github Logo"></a></p></div>
+					if($disponivel){
+						echo "<p style='color:green;'>The <u>$domain</u> domain is available to register!</p><br>";
+					}else{
+						echo "<p style='color:red;'>The <u>$domain</u> domain is not available!</p><br>";
+					}
+				}
+				?>
+				
+				<p><a class="backbtn" href="javascript:history.back()">BACK</a></p><br />
+			</div>
+		</div>
+		<a href="https://github.com/jancordeiro" target="_blank" class="github"><i class="fa-brands fa-github"></i></a>
+	</div>	
 </body>
 </html>
